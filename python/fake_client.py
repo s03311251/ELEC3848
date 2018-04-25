279e05d70871cb05e6db3939ddf8a39efbd9fb54
BananaShinshi <s03311251@hotmail.com>
2018-04-25 16:05:17 +0800

    server works

---
+++ python/fake_client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import socket
+
+if __name__ == '__main__':
+
+	TCP_IP = 'localhost'
+	TCP_PORT = 38763
+	BUFFER_SIZE = 1024
+	'''
+	TOILET_ID = 2
+	STALL_NUM = 5
+	stall_state = ['0'] * STALL_NUM
+	wait_t_buffer = [15, 25]
+	MSG_HEADER = str(TOILET_ID) + ' ' + str(STALL_NUM)
+	'''
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	'''
+	msg = MSG_HEADER
+
+	for i in stall_state:
+		msg += ' ' + i
+
+	while len(wait_t_buffer) > 0:
+		msg += ' ' + str(wait_t_buffer.pop())
+	'''
+
+	msg = '1 2 1 0 10\nC 1 0 0 0 15 0 0 0\nB 3.14\n'
+
+	s.send(msg.encode('utf-8'))
+	#data = s.recv(BUFFER_SIZE)
+	s.close()
+
+	#print ("received data:", data)
--- python/server.py
+++ python/server.py
@@ -11,6 +11,7 @@ from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 
 LOG_MAX_LEN = 500
 LOG_T_DELTA = datetime.timedelta(minutes=5)
+employee_name = ['Alice', 'Bob', 'Christina', 'Dave']
 
 
 
@@ -41,11 +42,12 @@ def ToiletPlot():
 	t_list = [float(i) for i in t_list]
 
 	plt.hist(t_list, normed=False, bins=100) # bins: divide into 100 ranges
-	
+
 	plt.title('Average pooping time')
 	plt.ylabel('frequency')
 	plt.xlabel('time (sec)')
 	plt.savefig('data/toilet1_plot.png')
+	plt.clf()
 
 
 
@@ -91,11 +93,12 @@ def CubiclePlot(worker):
 	t_list = [float(i) for i in t_list]
 
 	plt.hist(t_list, normed=False, bins=100) # bins: divide into 100 ranges
-	
-	plt.title('Average pooping time for worker '+str(worker))
+
+	plt.title('Average pooping time for '+employee_name[worker-1])
 	plt.ylabel('frequency')
 	plt.xlabel('time (sec)')
 	plt.savefig('data/cubicle'+str(worker)+'_plot.png')
+	plt.clf()
 
 
 
@@ -134,44 +137,44 @@ def CubicleHourPlot(worker):
 	plt.bar(np.arange(0,24), ratio_list, align='center')
 
 	plt.xlim([-0.5,23.5])
-	plt.title('Working time of worker '+str(worker))
+	plt.title('Working time of worker '+employee_name[worker-1])
 	plt.ylabel('percentage')
 	plt.xlabel('time (hour)')
 	plt.savefig('data/cubicle'+str(worker)+'_hour_plot.png')
-
+	plt.clf()
 
 
 def BatteryRecord(x):
 	now_t = datetime.datetime.now()
 
-	with open ("battery_log.txt", "r") as f:
+	with open ("data/battery_log.txt", "r") as f:
 		lines = f.readlines()
 	f.close()
 
 	if len(lines) > 0:
 		last_log = lines[-1].split(',')
-		last_t = datetime.datetime.strptime(last_log[0], '%Y-%m-%d %I:%M:%S.%f')
+		last_t = datetime.datetime.strptime(last_log[0], '%Y-%m-%d %H:%M:%S.%f')
 	else:
 		last_t = now_t
 
 	# remove lines if necessiary
 	if len(lines) > LOG_MAX_LEN:
 		if now_t - last_t < LOG_T_DELTA:
-			with open ("battery_log.txt", "w") as f:
+			with open ("data/battery_log.txt", "w") as f:
 				f.writelines(lines[-LOG_MAX_LEN:-1])
 			f.close()
 		else:
-			with open ("battery_log.txt", "w") as f:
+			with open ("data/battery_log.txt", "w") as f:
 				f.writelines(lines[-LOG_MAX_LEN:])
 			f.close()
 	else:
 		if now_t - last_t  < LOG_T_DELTA:
-			with open ("battery_log.txt", "w") as f:
+			with open ("data/battery_log.txt", "w") as f:
 				f.writelines(lines[:-1])
 			f.close()
 
 	# write
-	with open ("battery_log.txt", "a") as f:
+	with open ("data/battery_log.txt", "a") as f:
 		f.write(str(now_t)+','+str(x)+'\n')
 	f.close()
 
@@ -179,12 +182,12 @@ def BatteryRecord(x):
 
 def BatteryPlot():
 
-	with open ("battery_log.txt", "r") as f:
+	with open ("data/battery_log.txt", "r") as f:
 		r = csv.reader(f)
 		t_list = []
 		value_list = []
 		for row in r:
-			t_list.append(datetime.datetime.strptime(row[0], '%Y-%m-%d %I:%M:%S.%f'))
+			t_list.append(datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S.%f'))
 			value_list.append(row[1])
 	f.close()
 
@@ -205,15 +208,15 @@ def BatteryPlot():
 	plt.ylabel('fuel level')
 	plt.xlabel('time')
 	plt.savefig('data/battery_plot.png')
+	plt.clf()
 
 
 
 def TCPListener():
+	global toilet1_state, cubicle_state
 
 	TCP_PORT = 38763
 	BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
-
-	toilet_state = {}
 
 
 
@@ -235,33 +238,52 @@ def TCPListener():
 		conn.close()
 		print ("received data:", data_string)
 
-		if len(data_string) > 0:
-			words = data_string.decode('utf-8').split(' ')
+		command_list = data_string.decode('utf-8').split('\n')
+		for command in command_list:
+			if len(command) > 0:
+				print (command)
+				words = command.split(' ')
 
-			if words[0] == 'B':
-				BatteryRecord(words[1])
+				if words[0] == 'B':
+					if len(words) >= 2:
+						BatteryRecord(words[1])
+					else:
+						print ('Error: B msg not complete')
 
-			else:
-				toilet_id = words[0]
-				stall_num = int(words[1])
-				stall_state = []
-				for word in words[2:2+stall_num]:
-					stall_state.append(word == '0')
-				wait_t = []
-				for word in words[2+stall_num:]:
-					wait_t.append(int(word))
 
-				print(toilet_id)
-				print(stall_state)
-				print(wait_t)
+				elif words[0] == 'C':
+					if len(words) >= 9:
+						cubicle_state = [i == '1' for i in words[1:5]]
+						cubicle_t = [int(i) for i in words[5:9]]
+						for i in [1,2,3,4]:
+							CubicleHourRecord(i, cubicle_state[i-1])
+							if cubicle_t[i-1] != 0:
+								CubicleRecord(i, cubicle_t[i-1])
+					else:
+						print ('Error: C msg not complete')
 
-				if toilet_id ==  '1':
-					toilet1_state == stall_state
-				for i in wait_t:
-					ToiletRecord(i)
+				else:
+					toilet_id = words[0]
+					stall_num = int(words[1])
+					stall_state = []
+					for word in words[2:2+stall_num]:
+						stall_state.append(word == '0')
+					wait_t = []
+					for word in words[2+stall_num:]:
+						wait_t.append(int(word))
+
+					print(toilet_id)
+					print(stall_state)
+					print(wait_t)
+
+				if toilet_id == '1':
+					toilet1_state = stall_state
+					for i in wait_t:
+						ToiletRecord(i)
 
 
 toilet1_state = []
+cubicle_state = []
 manager_set = set()
 PASSWORD = 'elpsycongroo'
 
@@ -270,15 +292,23 @@ PASSWORD = 'elpsycongroo'
 def on_chat_message(msg):
 	global manager_set
 
-	menu_vacancy = InlineKeyboardMarkup(inline_keyboard=[
+	menu_toilet = InlineKeyboardMarkup(inline_keyboard=[
 		[InlineKeyboardButton(text='1/F', callback_data='toilet 1')],
 		[InlineKeyboardButton(text='2/F', callback_data='toilet 2')],
 		[InlineKeyboardButton(text='3/F', callback_data='toilet 3')],
 	])
 
+	menu_su = InlineKeyboardMarkup(inline_keyboard=[
+		[InlineKeyboardButton(text='Toilet Vacancy', callback_data='su toilet')],
+		[InlineKeyboardButton(text='Toilet Stat', callback_data='su toilet_stat')],
+		[InlineKeyboardButton(text='Cubicle Condition', callback_data='su cubicle')],
+		[InlineKeyboardButton(text='Cubicle Stat', callback_data='su cubicle_stat')],
+		[InlineKeyboardButton(text='Battery Stat', callback_data='su battery')],
+	])
+
 	content_type, chat_type, chat_id = telepot.glance(msg)
 	from_id = msg['from']['id']
-
+	
 	if content_type == 'text':
 		commands = msg['text'].strip().lower().split(' ')
 
@@ -298,20 +328,63 @@ def on_chat_message(msg):
 					if commands[1] == '1':
 						with open('data/toilet1_plot.png', 'rb') as f:
 							bot.sendPhoto(from_id, f)
+						f.close()
+					elif commands[1] == 'c':
+						if len(cubicle_state) == 0:
+							bot.sendMessage(from_id, '*Cubicle*\nNo data yet', parse_mode='Markdown')
+						else:
+							reply_str = '*Cubicle*\n'
+
+							# check state
+							i = 0
+							for state in cubicle_state:
+								reply_str += employee_name[i] + ': '
+								if state == True:
+									reply_str += 'Present\n'
+								else:
+									reply_str += 'Absent\n'
+								i += 1
+
+							bot.sendMessage(from_id, reply_str, parse_mode='Markdown')
 				else:
 					bot.sendMessage(from_id, 'Usage: /stat <floor>')
 			else:
 				bot.sendMessage(from_id, 'Error: Not in Manager Mode')
 
+		elif from_id in manager_set:
+			bot.sendMessage(from_id, 'Manager Menu:', reply_markup = menu_su)
 		else:
-			bot.sendMessage(from_id, 'Which floor?', reply_markup = menu_vacancy)
+			bot.sendMessage(from_id, 'Which floor?', reply_markup = menu_toilet)
 
 
 
 def on_callback_query(msg):
+
+	menu_toilet = InlineKeyboardMarkup(inline_keyboard=[
+		[InlineKeyboardButton(text='1/F', callback_data='toilet 1')],
+		[InlineKeyboardButton(text='2/F', callback_data='toilet 2')],
+		[InlineKeyboardButton(text='3/F', callback_data='toilet 3')],
+	])
+
+	menu_toilet_stat = InlineKeyboardMarkup(inline_keyboard=[
+		[InlineKeyboardButton(text='1/F', callback_data='toilet_stat 1')],
+		[InlineKeyboardButton(text='2/F', callback_data='toilet_stat 2')],
+		[InlineKeyboardButton(text='3/F', callback_data='toilet_stat 3')],
+	])
+
+	menu_cubicle_stat = InlineKeyboardMarkup(inline_keyboard=[
+		[InlineKeyboardButton(text='Alice', callback_data='cubicle_stat 1')],
+		[InlineKeyboardButton(text='Bob', callback_data='cubicle_stat 2')],
+		[InlineKeyboardButton(text='Christina', callback_data='cubicle_stat 3')],
+		[InlineKeyboardButton(text='Dave', callback_data='cubicle_stat 4')],
+	])
+
+
+
 	query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
 	commands = query_data.split(' ')
 
+	# Toilet Menu
 	if commands[0] == 'toilet':
 		if commands[1] == '1':
 			if len(toilet1_state) == 0:
@@ -335,10 +408,88 @@ def on_callback_query(msg):
 				if all_occupied:
 					bot.sendMessage(from_id, 'No stall is vacant\nAverage Waiting Time: '+str(ToiletAvg(1)))
 					bot.sendMessage(from_id, 'You may go to the toilet on 2/F\nVacant stalls: 1, 4, 5')
+
 		elif commands[1] == '2':
 			bot.sendMessage(from_id, '*2/F*\n1: Vacant\n2: Occupied\n3: Occupied\n4: Vacant\n5: Vacant\n', parse_mode='Markdown')
+
 		elif commands[1] == '3':
 			bot.sendMessage(from_id, '*3/F*\n1: Vacant\n2: Occupied\n3: Occupied\n4: Occupied\n', parse_mode='Markdown')
+
+
+
+	# Manager Menu
+	elif from_id not in manager_set:
+				bot.sendMessage(from_id, 'Error: Not in Manager Mode')
+
+	elif commands[0] == 'su':
+		if commands[1] == 'toilet':
+			bot.sendMessage(from_id, 'Which floor?', reply_markup = menu_toilet)
+
+		elif commands[1] == 'toilet_stat':
+			bot.sendMessage(from_id, 'Which floor?', reply_markup = menu_toilet_stat)
+
+		elif commands[1] == 'cubicle':
+			if len(cubicle_state) == 0:
+				bot.sendMessage(from_id, '*Cubicle*\nNo data yet', parse_mode='Markdown')
+			else:
+				reply_str = '*Cubicle*\n'
+				# check state
+				i = 0
+				for state in cubicle_state:
+					reply_str += employee_name[i] + ': '
+					if state == True:
+						reply_str += 'Present\n'
+					else:
+						reply_str += 'Absent\n'
+					i += 1
+				bot.sendMessage(from_id, reply_str, parse_mode='Markdown')
+
+		elif commands[1] == 'cubicle_stat':
+			bot.sendMessage(from_id, 'Which Employee?', reply_markup = menu_cubicle_stat)
+
+		elif commands[1] == 'battery':
+			bot.sendMessage(from_id, 'Drawing Plot')
+			BatteryPlot()
+
+			bot.sendMessage(from_id, 'Sending Plot')
+			with open('data/battery_plot.png', 'rb') as f:
+				bot.sendPhoto(from_id, f)
+			f.close()
+
+
+
+	elif commands[0] == 'toilet_stat':
+		bot.sendMessage(from_id, 'Drawing Plot')
+		if commands[1] == '1':
+			ToiletPlot()
+
+		bot.sendMessage(from_id, 'Sending Plot')
+		with open('data/toilet'+commands[1]+'_plot.png', 'rb') as f:
+			bot.sendPhoto(from_id, f)
+		f.close()
+
+
+
+	elif commands[0] == 'cubicle_stat':
+
+		bot.sendMessage(from_id, 'Drawing Plot')
+		CubicleHourPlot(int(commands[1]))
+		CubiclePlot(int(commands[1]))
+
+
+		bot.sendMessage(from_id, 'Sending Plot')
+		with open('data/cubicle'+commands[1]+'_plot.png', 'rb') as f:
+			bot.sendPhoto(from_id, f)
+		f.close()
+		with open('data/cubicle'+commands[1]+'_hour_plot.png', 'rb') as f:
+			bot.sendPhoto(from_id, f)
+		f.close()
+
+
+
+
+
+
 
 
 
